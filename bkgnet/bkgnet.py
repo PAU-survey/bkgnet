fix flux scaling

bkgnet/bkgnet.py
@@ -116,8 +116,9 @@ class BKGnet:
                 outputs = self.cnn(bstamp, bx, by, bmax_flux, bband, std)
 
             # The network gives the error in log(error)
-            outputs[:,1] = torch.exp(outputs[:,1]) 
-            pred.append(std[:,None]*outputs.squeeze())
+            outputs[:,0] = std[:,None]*outputs[:,0] + mean[:,None]
+            outputs[:,1] = std[:,None]*torch.exp(outputs[:,1]) 
+            pred.append(outputs.squeeze())
         
         pred = pd.DataFrame(torch.cat(pred).detach().numpy(), \
                             index=ps_info.index, columns=['bkg', 'bkg_error'])